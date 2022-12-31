52eed1b761429bdf1282929dc4630b934da90e0b
Abhishek Kumar <abhishek02102003@gmail.com>
2022-12-31 22:55:48 +0530

    build auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 
 // database connection
@@ -28,6 +29,7 @@ connection.once('open', () => {
 }).on("error", (err) => {
     console.log('Connection failed due to:', err)
 })
+
 
 
 
@@ -51,12 +53,26 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } /* 24 hours */
 }))
 
+
+
+
+// passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
 app.use(flash())
+app.use(express.urlencoded({extended:false}))
 app.use(express.json()) 
+
 
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
